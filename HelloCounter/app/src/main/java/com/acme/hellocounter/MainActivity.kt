7450ba346afbe08026123da4984cd1e2ccc45adf
AZIZ-Soufiane <aziz.soufiane.solicode@gmail.com>
2025-10-02 10:56:41 +0100

    5.1.8 - Final-touches

--- HelloCounter/app/src/main/java/com/acme/hellocounter/MainActivity.kt
+++ HelloCounter/app/src/main/java/com/acme/hellocounter/MainActivity.kt
@@ -12,11 +12,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.acme.hellocounter.ui.MainScreen
+import com.acme.hellocounter.ui.theme.HelloCounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MaterialTheme { MainScreen() } }
+        setContent { HelloCounterTheme { MainScreen() } }
     }
 }
 
